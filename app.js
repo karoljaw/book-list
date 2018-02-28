9a4fad108b49b56d2dc8f29bfff4fd7b8dfb681b
Karol Jaworek <karoljaworek04@gmail.com>
2018-02-28 09:47:22 +0100

    local storage added

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ function Book(title, author, isbn) {
 }
 //declare UI object
 function UI() { };
+
 //add book prototype
 UI.prototype.addBookToList = function (book) {
     const bookList = document.getElementById("book-list");
@@ -44,6 +45,65 @@ UI.prototype.showAlert = function(message, className) {
         div.style.display = "none";
     }, 3000);
 }
+//get books from local storage
+UI.prototype.getBooksFromLocalStorage = function(book) {
+    let books;
+    if (localStorage.getItem("books") === null) {
+        books = [];
+    } else {
+        books = JSON.parse(localStorage.getItem("books"));
+    }
+
+    books.forEach(function(book) {
+        const bookList = document.getElementById("book-list");
+    const tr = document.createElement("tr");
+    tr.innerHTML = `
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.isbn}</td>
+    <td><a href="#" class="delete">X</a></td>`;
+    bookList.appendChild(tr);
+    })
+
+    localStorage.setItem("books", JSON.stringify(books));
+}
+// add to ls prototype
+UI.prototype.addToLocalStorage = function(book) {
+    let books;
+    if (localStorage.getItem("books") === null) {
+        books = [];
+    } else {
+        books = JSON.parse(localStorage.getItem("books"));
+    }
+
+    books.push(book);
+
+    localStorage.setItem("books", JSON.stringify(books));
+}
+//remove from ls
+UI.prototype.removeBookFromLocalStorage = function(isbn) {
+    let books;
+    if (localStorage.getItem("books") === null) {
+        books = [];
+    } else {
+        books = JSON.parse(localStorage.getItem("books"));
+    }
+
+   books.forEach(function(book, index){
+       if (book.isbn === isbn) {
+           books.splice(index, 1);
+       }
+   })
+
+   books = localStorage.setItem("books", JSON.stringify(books));
+}
+// get books DOM event listener
+document.addEventListener("DOMContentLoaded", function() {
+    const ui = new UI();
+    ui.getBooksFromLocalStorage();
+});
+
+
 //add book event listener
 document.querySelector("#book-form").addEventListener("submit", function (e) {
     const title = document.querySelector("#title").value,
@@ -61,13 +121,15 @@ document.querySelector("#book-form").addEventListener("submit", function (e) {
         ui.clearFields();
 
         ui.showAlert(`Book added`, `success`);
+
+        ui.addToLocalStorage(book);
     }
 
-    
     e.preventDefault();
 })
 //remove book event listener
 document.querySelector("#book-list").addEventListener("click", function (e) {
     const ui = new UI();
     ui.removeItem(e.target);
+    ui.removeBookFromLocalStorage(e.target.parentElement.previousElementSibling.textContent);
 })
